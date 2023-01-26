60ac34761a7fa776df0e1dce4c9f4cf0ca274766
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-27 01:52:17 +0300

    add 2 work

---
+++ 2work/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+int number = new Random().Next(1, 100000);
+int sumN = 0;
+Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int n = number % 10;
+    sumN = sumN + n;
+    number = number / 10;
+}
+
+Console.WriteLine(sumN);
+Console.WriteLine();
