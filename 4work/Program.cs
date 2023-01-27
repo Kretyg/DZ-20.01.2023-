714035127ddf25f972030c16a541bd65fbf601f8
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-27 06:36:53 +0300

    иправил и доделал

---
+++ 4work/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+Console.WriteLine("Четные");
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i} ^ 3 = {i*i*i}");
+        i++;
+    }
+    else if (i % 2 == 1)
+    {
+        i++;
+    }
+}
