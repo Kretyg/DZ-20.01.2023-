58799d650656975c6fc4e7c56f96c7af682485db
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-27 06:36:38 +0300

    иправил и доделал

--- 3work/Program.cs
+++ 3work/Program.cs
@@ -2,5 +2,9 @@
 
 Console.Write("Введите число: ");
 int N = int.Parse(Console.ReadLine()!);
-
-int ()
+int sum = 1;
+for(int i = 1; i <= N; i++)
+{
+    sum=sum * i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {sum}");
